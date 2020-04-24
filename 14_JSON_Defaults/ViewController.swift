ea97d135bbc35371e9f381557b5acbf0dd8d04d7
Mykyta Pohorielov <pohorielov.m@gmail.com>
2020-04-24 22:56:44 +0100

    Initial Commit

---
+++ 14_JSON_Defaults/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  14_JSON_Defaults
+//
+//  Created by Mykyta Pohorielov on 24/04/2020.
+//  Copyright © 2020 Mykyta Pohorielov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
